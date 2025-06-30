6e50c70c5a1353f12110a5b5e0fb654a0c341b2f
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-06-30 09:53:56 -0700

    Fixed the layout problem!

--- apps/ui/src/layouts/ClientLayout.tsx
+++ apps/ui/src/layouts/ClientLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router"
 
 export default function ClientLayout() {
   return (
-    <div className="flex flex-col items-center justify-center bg-slate-4 h-dvh">
-      <div className="overflow-y-hidden bg-slate-1 border border-slate-6 h-dvh max-w-100 min-w-10 w-full flex flex-col">
+    <div className="flex flex-col items-center justify-center bg-slate-4 min-h-dvh">
+      <div className="overflow-y-hidden bg-slate-1 border border-slate-6 min-h-dvh max-w-100 min-w-10 w-full flex flex-col">
         <Outlet />
       </div>
     </div>
